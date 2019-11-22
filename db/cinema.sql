d9a51a30a7ddef7c0179aeb28b891ca8bd1adbf8
Grant Houston <grant.houston44@hotmail.com>
2019-11-22 16:07:33 +0000

    fixed bugs in sql file

--- db/cinema.sql
+++ db/cinema.sql
@@ -13,11 +13,17 @@ CREATE TABLE films(
   id SERIAL4 PRIMARY KEY,
   title VARCHAR,
   price DECIMAL(4, 2)
-)
+);
 
 
 CREATE TABLE screenings(
   id SERIAL4 PRIMARY KEY,
   showtimes VARCHAR,
-  film_id REFERENCES films(id)
-)
+  film_id INT REFERENCES films(id)
+);
+
+CREATE TABLE tickets(
+  id SERIAL4 PRIMARY KEY,
+  customer_id INT REFERENCES customers(id),
+  film_id INT REFERENCES films(id)
+);

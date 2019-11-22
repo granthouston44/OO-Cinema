422664b748c3b077871c7dff9062ca628bb02d3a
Grant Houston <grant.houston44@hotmail.com>
2019-11-22 16:48:54 +0000

    tested all customers method

--- models/customer.rb
+++ models/customer.rb
@@ -44,4 +44,10 @@ def update()
   SqlRunner.run(sql, values)
 end
 
+def self.all
+  sql = "SELECT * FROM customers"
+  customers = SqlRunner.run(sql)
+  return customers.map {|customer| Customer.new(customer)}
+end
+
 end

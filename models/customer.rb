fe15a9f026fdf70a6496a891dea97cea2b72e04c
Grant Houston <grant.houston44@hotmail.com>
2019-11-23 15:55:03 +0000

    customer films mehtod tested in pry

--- models/customer.rb
+++ models/customer.rb
@@ -3,51 +3,64 @@ require_relative('film')
 
 class Customer
 
-attr_reader :id
-attr_accessor :name, :wallet
+  attr_reader :id
+  attr_accessor :name, :wallet
 
-def initialize(customer_details)
-  @id = customer_details['id'].to_i if customer_details['id']
-  @name = customer_details['name']
-  @wallet = customer_details['wallet'].to_i
-end
+  def initialize(customer_details)
+    @id = customer_details['id'].to_i if customer_details['id']
+    @name = customer_details['name']
+    @wallet = customer_details['wallet'].to_i
+  end
 
-def save()
-  sql = "
-  INSERT INTO customers
-  (name, wallet)
-  VALUES ($1, $2)
-  RETURNING id"
-  values = [@name, @wallet]
-  customer = SqlRunner.run(sql, values).first
-  @id = customer['id'].to_i
-end
+  def save()
+    sql = "
+    INSERT INTO customers
+    (name, wallet)
+    VALUES ($1, $2)
+    RETURNING id"
+    values = [@name, @wallet]
+    customer = SqlRunner.run(sql, values).first
+    @id = customer['id'].to_i
+  end
 
-def self.delete_all
-  sql = "DELETE FROM customers"
-  SqlRunner.run(sql)
-end
+  def self.delete_all
+    sql = "DELETE FROM customers"
+    SqlRunner.run(sql)
+  end
 
-def delete()
-  sql = "DELETE FROM customers where id = $1"
-  values = [@id]
-  SqlRunner.run(sql, values)
-end
+  def delete()
+    sql = "DELETE FROM customers where id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
 
-def update()
-  sql = "
-  UPDATE customers
-  SET (name, wallet) =
-  ($1, $2)
-  WHERE id = $3"
-  values = [@name, @wallet, @id]
-  SqlRunner.run(sql, values)
-end
+  def update()
+    sql = "
+    UPDATE customers
+    SET (name, wallet) =
+    ($1, $2)
+    WHERE id = $3"
+    values = [@name, @wallet, @id]
+    SqlRunner.run(sql, values)
+  end
 
-def self.all
-  sql = "SELECT * FROM customers"
-  customers = SqlRunner.run(sql)
-  return customers.map {|customer| Customer.new(customer)}
-end
+  def self.all
+    sql = "SELECT * FROM customers"
+    customers = SqlRunner.run(sql)
+    return customers.map {|customer| Customer.new(customer)}
+  end
+
+  def film_tickets()
+    sql = "
+    SELECT films.* FROM films
+    INNER JOIN tickets ON tickets.film_id =
+    films.id WHERE customer_id = $1
+    "
+    values = [@id]
+    result = SqlRunner.run(sql, values)
+
+    return nil if result.count == 0
+    return result.map {|film| Film.new(film)}
+  end
 
 end

223551c113f1c4d90569ed1102fe7eaf1361ac8d
Grant Houston <grant.houston44@hotmail.com>
2019-11-23 14:59:58 +0000

    films update method tested in pry

--- models/film.rb
+++ models/film.rb
@@ -42,6 +42,16 @@ def self.all
   return result
 end
 
+def update
+  sql = "
+  UPDATE films
+  SET (title, price) =
+  ($1, $2)
+  WHERE id = $3
+  "
+  values = [@title, @price, @id]
+  SqlRunner.run(sql, values)
+end
 
 
 
